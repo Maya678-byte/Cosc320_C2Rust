c5bb1b7d434b969577af4c3b07f1635f58034c1c
Maya678-byte <elfoulymaya182@gmail.com>
2025-05-02 14:14:23 +0400

    Create testsLexer.rs

---
+++ testsLexer.rs
@@ -0,0 +1,24 @@
+#[test]
+fn test_lexer_basic_keywords() {
+    use std::ptr;
+    extern "Rust" {
+        static mut P: *mut i8;
+        static mut LINE: i32;
+        static mut TK: i32;
+        static mut IVAL: i32;
+        fn next_token();
+    }
+
+    unsafe {
+        let src = b"return 42;\0";
+        P = src.as_ptr() as *mut i8;
+        LINE = 1;
+        next_token();
+        assert_eq!(TK, 139); // Token::Return
+        next_token();
+        assert_eq!(TK, 128); // Token::Num
+        assert_eq!(IVAL, 42);
+        next_token();
+        assert_eq!(TK, ';' as i32);
+    }
+}

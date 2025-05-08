19f5490585cf93794c1801597423568e88f8bf2f
Maya678-byte <elfoulymaya182@gmail.com>
2025-05-08 15:40:15 +0400

    Update c4.rs

--- src/c4.rs
+++ src/c4.rs
@@ -788,40 +788,6 @@ fn err(msg: &str) {
         *self.e = Opcode::ADJ as i32;
         self.e = self.e.add(1);
         *self.e = 1;
-
-
-
-//Test cases
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_tokenizer_with_simple_if() {
-        unsafe {
-            let source = b"if (1) return 0;\0";
-            P = source.as_ptr() as *mut i8;
-            LINE = 1;
-            next_token();
-            assert_eq!(TK, Token::If as i32);
-            next_token();
-            assert_eq!(TK, b'(' as i32);
-            next_token();
-            assert_eq!(TK, Token::Num as i32);
-            assert_eq!(IVAL, 1);
-            next_token();
-            assert_eq!(TK, b')' as i32);
-            next_token();
-            assert_eq!(TK, Token::Return as i32);
-            next_token();
-            assert_eq!(TK, Token::Num as i32);
-            assert_eq!(IVAL, 0);
-            next_token();
-            assert_eq!(TK, b';' as i32);
-        }
-    }
-}
-
         self.e = self.e.add(1);
     }
 }

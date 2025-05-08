d5cd544e400bfbee8ce4fedede0ec94e55b92574
Maya678-byte <elfoulymaya182@gmail.com>
2025-05-08 14:41:30 +0400

    moved 1st test to main.rs

--- src/main.rs
+++ src/main.rs
@@ -831,3 +831,34 @@ fn main() {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tokenizer_with_simple_if() {
+        unsafe {
+            let source = b"if (1) return 0;\0";
+            P = source.as_ptr() as *mut i8;
+            LINE = 1;
+            next_token();
+            assert_eq!(TK, Token::If as i32);
+            next_token();
+            assert_eq!(TK, b'(' as i32);
+            next_token();
+            assert_eq!(TK, Token::Num as i32);
+            assert_eq!(IVAL, 1);
+            next_token();
+            assert_eq!(TK, b')' as i32);
+            next_token();
+            assert_eq!(TK, Token::Return as i32);
+            next_token();
+            assert_eq!(TK, Token::Num as i32);
+            assert_eq!(IVAL, 0);
+            next_token();
+            assert_eq!(TK, b';' as i32);
+        }
+    }
+}
+
